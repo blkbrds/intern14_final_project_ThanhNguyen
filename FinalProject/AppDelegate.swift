acf90293a042fb680e2d8eb2c939e518dab5fb68
Thanh Nguyen X.4 <thanh.nguyen4@asiantech.vn>
2019-09-16 09:53:42 +0700

    config tabbar controller

--- FinalProject/AppDelegate.swift
+++ FinalProject/AppDelegate.swift
@@ -13,22 +13,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    // MARK: - Singleton
+    static let shared: AppDelegate = {
+        guard let shared = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Cannot cast `UIApplication.shared.delegate` to `AppDelegate`.")
+        }
+        return shared
+    }()
+
+    enum ViewController {
+        case tabbar
+    }
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        configTabbar(controller: .tabbar)
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
+// MARK: - Config Tabbar
+    func configTabbar(controller: ViewController) {
+        switch controller {
+        case .tabbar:
+            // Home:
+            let homeViewController = HomeViewController()
+            let homeNavigationController = UINavigationController(rootViewController: homeViewController)
+            homeNavigationController.tabBarItem = UITabBarItem(title: "Home", image: #imageLiteral(resourceName: "ic-home"), selectedImage: #imageLiteral(resourceName: "ic-home-selected"))
+
+            let searchViewController = SearchViewController()
+            let searchNavigationController = UINavigationController(rootViewController: searchViewController)
+            searchNavigationController.tabBarItem = UITabBarItem(title: "Search", image: #imageLiteral(resourceName: "ic-search"), selectedImage: #imageLiteral(resourceName: "ic-search-selected"))
+
+            let favoritesViewController = FavoritesViewController()
+            let favoritesNavigationController = UINavigationController(rootViewController: favoritesViewController)
+            favoritesNavigationController.tabBarItem = UITabBarItem(title: "Favorites", image: #imageLiteral(resourceName: "ic-favorites"), selectedImage: #imageLiteral(resourceName: "ic-favorites-selected"))
+
+            let viewControllers = [homeNavigationController, searchNavigationController, favoritesNavigationController]
+            let tabbarController = UITabBarController()
+            tabbarController.viewControllers = viewControllers
+
+            window?.rootViewController = tabbarController
+            window?.makeKeyAndVisible()
+        }
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-    }
+// MARK: - Application Life Cycle
+    func applicationWillResignActive(_ application: UIApplication) { }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    }
+    func applicationDidEnterBackground(_ application: UIApplication) { }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-    }
+    func applicationWillEnterForeground(_ application: UIApplication) { }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-    }
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+
+    func applicationWillTerminate(_ application: UIApplication) { }
 }
---
+++ FinalProject/View/Controller/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  FinalProject
+//
+//  Created by MBA0158 on 9/16/19.
+//  Copyright © 2019 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: ViewController {
+
+    // MARK: - Outlet
+    @IBOutlet private weak var tableView: TableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
