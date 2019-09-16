d7f0c593dd639682f9f2d36147780de73fd2f3b6
Thanh Nguyen X.4 <thanh.nguyen4@asiantech.vn>
2019-09-16 14:23:52 +0700

    Update UI for header HomeVC

---
+++ FinalProject/View/Controller/Home/Header/CollectionCell/HeaderCollectionCell.swift
@@ -0,0 +1,33 @@
+//
+//  HeaderCollectionCell.swift
+//  FinalProject
+//
+//  Created by MBA0158 on 9/16/19.
+//  Copyright © 2019 Asiantech. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+final class HeaderCollectionCell: CollectionCell {
+// MARK: - Outlet
+    @IBOutlet private weak var headerLabel: Label!
+    @IBOutlet private weak var headerImage: ImageView!
+
+    var viewModel: HeaderCollectionCellViewModel? {
+        didSet {
+            updateView()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func updateView() {
+        guard let viewModel = viewModel else { return }
+        headerLabel.text = viewModel.headerName
+        headerImage.sd_setImage(with: URL(string: viewModel.imageURL), placeholderImage: #imageLiteral(resourceName: "ic-search-selected"))
+    }
+}
---
+++ FinalProject/View/Controller/Home/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  FinalProject
+//
+//  Created by MBA0158 on 9/16/19.
+//  Copyright © 2019 Asiantech. All rights reserved.
+//
+
+import Foundation
+import MVVM
+
+final class HomeViewModel: MVVM.ViewModel {
+    // MARK: - Propeties
+}
